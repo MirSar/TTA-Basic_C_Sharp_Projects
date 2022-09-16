f8e1c2c471e68b335c92d5de7ef58f57feaf1c16
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-09-15 19:48:21 -0700

    committing basic c# codes

---
+++ HelloWorldConsoleApp/HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// using only the basic format
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        // Console.ReadLine() tells the console to stay open until we press enter
+        Console.ReadLine();
+    }
+}
+
---
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            // Console.ReadLine() keeps the console window open
+            // it makes the console read the input, then display it
+            string name = Console.ReadLine();
+            Console.WriteLine("Peace, " + name + "!");
+            Console.ReadLine();
+        }
+    }
+}
