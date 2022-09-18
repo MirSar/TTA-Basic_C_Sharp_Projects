d18a3686376c470349698e565efc886eacb7b892
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-09-18 13:38:14 -0700

    Console App: Daily Report

---
+++ DailyReportApp.cs/DailyReportApp.cs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DailyReportApp.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy.");
+            Console.WriteLine("Student Daily Report.");
+            Console.WriteLine("======================================");
+            Console.WriteLine("Please answer the following questions:");
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+            
+            Console.WriteLine("What page number?");
+            string ipageNum = Console.ReadLine();
+            ushort pageNum = Convert.ToUInt16(ipageNum);
+
+            Console.WriteLine("Do you need help with anything? \nPlease answer \"true\" or \"false\" ");
+            string ineedHelp = Console.ReadLine();
+            bool needHelp = Convert.ToBoolean(ineedHelp);
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? \n Please give specifics.");
+            string posExperience = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? \n Please give specifics.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study?");
+            string ihoursStudied = Console.ReadLine();
+            byte hoursStudied = Convert.ToByte(ihoursStudied);
+
+            Console.WriteLine("*********************************");
+            Console.WriteLine("Thank you for your answers.\n" +
+                "An Instructor will respond to this shortly.\n" +
+                "Have a great day!");
+            Console.WriteLine("*********************************");
+
+            // Testing user input via output
+            Console.WriteLine("Checking Output:");
+            Console.WriteLine(yourName);
+            Console.WriteLine(currentCourse);
+            Console.WriteLine(pageNum);
+            Console.WriteLine(needHelp);
+            Console.WriteLine(posExperience);
+            Console.WriteLine(feedback);
+            Console.WriteLine(hoursStudied);
+
+            // keep the Console open
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// asking for name
+            //Console.WriteLine("What is your name? ");
+            //string yourName = Console.ReadLine();
+            //// using user input
+            //Console.WriteLine("Greetings " + yourName + ". How may I be of service?");
+            //// keep console open
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //// Convert datatype from string to int
+            //int fNum1 = Convert.ToInt32(favoriteNumber);
+            //// math operation
+            //int fNum = 114 + fNum1;
+            //// output
+            //Console.WriteLine("Your favorite number plus 114 is " + fNum);
+            //Console.ReadLine();
+
+            //// byte takes integers from 0 to 255
+            //byte hoursworked = 42;
+            //bool isStudying = true;
+            //// character is one unicode character (+65 thoursnd)
+            //char questionMark = '\u2103';
+            //// signed byte -128 to 127
+            //sbyte currenttemp = -23;
+            //short temperatureonmars = -341;
+            //float secondsleft = 2.624567f;
+            //// int whole numbers +-2billion
+            //int starsingallaxies = 200000000;
+            //uint biggallaxies = 400000000;
+            //// float with 14-15 digits
+            //double heightincentimeters = 21.4654;
+            //long bignum = 123456789112345678;
+            //// largest datatype decimal (28-29 digits in length)
+            //// for decimal use suffix 'm'
+            //decimal moneyinbank = 100.5m;
+            //// output
+            //Console.WriteLine(questionMark);
+            //Console.WriteLine(isStudying);
+            //Console.WriteLine(hoursworked);
+            //Console.WriteLine(currenttemp);
+            //Console.WriteLine(temperatureonmars);
+            //Console.WriteLine(secondsleft);
+            //Console.WriteLine(starsingallaxies);
+            //Console.WriteLine(biggallaxies);
+            //Console.WriteLine(heightincentimeters);
+            //Console.WriteLine(bignum);
+            //Console.WriteLine(moneyinbank);
+            //Console.ReadLine();
+
+            // Casting = converting one datatype to another
+            // It is best to avoid the need to cast
+            int currentAge = 45;
+            string yearsOld = currentAge.ToString();
+            Console.WriteLine("You are " + yearsOld + " years young");
+            Console.ReadLine();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine("raining? " + rainingStatus);
+            Console.ReadLine();
+
+
+        }
+    }
+}
