25895c8143503fe42f247ffe0cbd9c5c75736219
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-09-25 11:39:38 -0700

    Assignment submission - Method Class 3

---
+++ Assignment_MethodClass3/Assignment_MethodClass3/MethodClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_MethodClass3
+{
+    public class MethodClass
+    {
+        public void methodClass1(int num1, int num2, out int total)
+        {
+            // Math operation on the first integer
+            total = num1 + num2;
+
+            // display the second integer to the screen
+            Console.WriteLine("The second integer is {0}", num2);
+           
+        }
+    }
+}
---
+++ Assignment_MethodClass3/Assignment_MethodClass3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Assignment_MethodClass3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("Assignment Method Class Submission");
+            Console.WriteLine("*****************************************");
+
+            // instantiating the class
+            MethodClass methodClass = new MethodClass();
+
+            int total;
+            // Call the method in the class, passing in two numbers.
+            methodClass.methodClass1(13, 9, out total);
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("If the sum of the two integers is {0}\n" +
+                "What is the first integer?", total);
+            Console.WriteLine("-----------------------------------");
+
+
+            // Call the method in the class, specifying the parameters by name.
+            methodClass.methodClass1(num1: 27, num2: 114, out total);
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("If the sum of the two integers is {0}\n" +
+                "What is the first integer", total);
+            Console.WriteLine("-----------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
