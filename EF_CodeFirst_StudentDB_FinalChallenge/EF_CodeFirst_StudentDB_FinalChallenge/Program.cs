ade976f77f3eb532b14aa7f1d279346d4f60e216
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-10-26 12:20:08 -0700

    C# Final Challenge: using Entity Framework 6 Code-First approach Student database

---
+++ EF_CodeFirst_StudentDB_FinalChallenge/EF_CodeFirst_StudentDB_FinalChallenge/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace EF_CodeFirst_StudentDB_FinalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Final Challenge\nConsole Application of Student database\n" +
+                "Using: Entity Framework 6 - Code First approach");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Software Developer: Mirwais Sarwary");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("\n");
+
+            // 2. Add a student using context class
+            using (var ctx = new SchoolContext())
+            {
+                // initializing student
+                var stud = new Student()
+                {
+                    StudentName = "Mirwais",
+                    StudentID = 001,
+                    DateOfBirth= new DateTime(1977,3,8),
+                    Height=5.8m,
+                    Weight= 180               
+                };
+                // Adding Student into the database
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+
+            // Keep the console Open
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        // 1. Create required Classes
+        //Initial domain classes- Creating Student Class
+        public class Student
+        {
+            // Properties
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+            public byte[] Photo { get; set; }
+            public decimal Height { get; set; }
+            public float Weight { get; set; }
+            // One-to-many relationship with class Grade
+            public Grade Grade { get; set; }
+
+        }
+        // Initial domain classes- Creating Grade Class
+        public class Grade
+        {
+            // Properties
+            public int GradeId { get; set; }
+            public string GradeName { get; set; }
+            public string Section { get; set; }
+            // One-to-many relationship with class Student
+            public ICollection<Student> Students { get; set; }
+        }
+        // Code-first requires a context class (derived from DbContext class)
+        // creates the database based on the parameter passed in the base constructor of your context class.
+        public class SchoolContext: DbContext
+        {
+            // If parameter is not passed in the constructor of our context class, it automatically creates
+            // required database in the local SQLEXPRESS database
+            public SchoolContext(): base()
+            {
+            }
+            // Using DbContext DbSet properties for our classes (part of our model)
+            // Note: DbSet is a collection of entity classes
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Grade> Grades { get; set; }
+        }
+
+    }
+}
---
+++ Scores/Scores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back {uName}. Today is {date}.";
+            Console.WriteLine(msg);
+
+            // Using StudentScore file to calculate the average score
+            // file path
+            string path = @"C:\Users\mirwa\OneDrive\Documents\2 GitHub\TTA-Basic_C_Sharp_Projects\Scores\Scores\studentScores.txt";
+            // Create an array to store our studentScores data
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            // initialize our score variable
+            double tScore = 0;
+            
+            Console.WriteLine("\nStudent Scores:\n");
+            // iterate the array
+            foreach (string line in lines)
+            {
+                // print the data on the console
+                Console.Write("\n" + line);
+                // Convert datatype for computation
+                double score = Convert.ToDouble(line);
+                // Calculate the total score
+                tScore += score;
+            }
+            // Find the average 
+            double avgScore = tScore / lines.Length;
+            Console.WriteLine("\nTotal of " + lines.Length + " student scores.\tAverage score: " + avgScore);
+
+            // keep the console open
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
